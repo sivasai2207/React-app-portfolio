f01a17e5b695ed483c1e5cfa7dcedd964ee354c2
Siva saikumar reddy k <49616092+sivasai2207@users.noreply.github.com>
2020-07-03 18:09:23 +0530

    changes..!

--- src/About.js
+++ src/About.js
@@ -37,7 +37,7 @@ const About = () => {
         <div className="text-center mt-4">
           <a
             className="btn btn-xl btn-outline-light"
-            href="https://startbootstrap.com/themes/freelancer/"
+            href="https://sivasaikumar.ml"
           >
             <i className="fas fa-download mr-2"></i>
             Download Resume..!
